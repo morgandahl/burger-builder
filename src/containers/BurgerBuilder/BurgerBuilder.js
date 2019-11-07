27fe9ce7cc450444bd6f8997d00c553071aae5d9
Morgan Christopher Dahl <Morgan.Dahl@familysearch.org>
2019-11-07 14:46:50 -0700

    So it begins...

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Aux from '../../hoc/Aux';
+
+const BurgerBuilder = () => {
+  return (
+    <Aux>
+      <div>Burger</div>
+      <div>Build Controls</div>
+    </Aux>
+  );
+}
+
+export default BurgerBuilder;
